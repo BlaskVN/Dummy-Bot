88c8199efd3dac29687c03f4ad017631018db86e
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2026-08-10 10:03:29 +0700

    feat(valorant): add tracker link commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,6 +4,7 @@ pub mod donation;
 pub mod general;
 pub mod moderation;
 pub mod presence;
+pub mod valorant;
 pub mod voice;
 pub mod word_puzzle;
 
@@ -18,5 +19,6 @@ pub fn all() -> Vec<poise::Command<Data, Error>> {
     commands.push(donation::donation());
     commands.push(presence::presence());
     commands.push(word_puzzle::word_puzzle());
+    commands.push(valorant::valorant());
     commands
 }
